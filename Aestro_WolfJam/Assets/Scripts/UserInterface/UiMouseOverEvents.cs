497522697d9a181cc5721e51f50306b6886d1c9e
Noah <noahm1216@gmail.com>
2024-06-08 10:17:30 -0700

    Audio samples for UI Mouse over

---
+++ Aestro_WolfJam/Assets/Scripts/UserInterface/UiMouseOverEvents.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UiMouseOverEvents : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    //public AudioSource aSource_Sfx;
+    //public AudioClip aClip_MouseOver;
+    //public AudioClip aClip_MouseExit;
+
+    public UnityEvent mouseOverEvent;
+    public UnityEvent mouseExitEvent;
+
+    private bool mouse_over = false;
+    void Update()
+    {
+        if (mouse_over)
+        {
+            //Debug.Log("Mouse Over");
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouse_over = true;
+        //Debug.Log("Mouse enter");
+        mouseOverEvent.Invoke();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouse_over = false;
+        //Debug.Log("Mouse exit");
+        mouseExitEvent.Invoke();
+    }
+}

ff129fd7655f117cbd7ffdd22a77d304f41cdc8c
Noah <noahm1216@gmail.com>
2024-06-09 11:36:01 -0700

    Update TutorialEvents.cs

--- Aestro_WolfJam/Assets/Scripts/Visuals/TutorialEvents.cs
+++ Aestro_WolfJam/Assets/Scripts/Visuals/TutorialEvents.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 public class TutorialEvents : MonoBehaviour
 {
 
+    public List<Light> spotlights = new List<Light>();
 
 
-    private IEnumerator Tutorial()
+
+    public IEnumerator Tutorial()
     {
         yield return new WaitForSeconds(0);
+    }
+
+    public IEnumerator LowerAWall(GameObject objectToLower)
+    {
+        yield return new WaitForSeconds(0);
+    }
+
+    public IEnumerator TurnDownLightIntensity()
+    {
+        if (spotlights.Count > 0)
+        {
+            foreach(Light spot in spotlights)
+            {
+                spot.intensity -= 1;
+                yield return new WaitForSeconds(0.05f);
+            }
+
+            if (spotlights[0].intensity > 0)
+                StartCoroutine(TurnDownLightIntensity());
+        }
+        
     }
 
 }// end of TutorialEvents class

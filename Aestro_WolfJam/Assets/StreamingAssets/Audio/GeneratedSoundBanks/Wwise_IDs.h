85695b133baad19139cd23cf7fa3f5231fd2edc8
Trisyd <gabrielpriem@gmail.com>
2024-06-08 21:50:15 -0700

    Text-to-Speech VO Wwise Setup

--- Aestro_WolfJam/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Aestro_WolfJam/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,10 +13,25 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID A_MOFF = 2661550753U;
+        static const AkUniqueID A_MON = 1685283445U;
+        static const AkUniqueID A_SELECTED = 1631326618U;
+        static const AkUniqueID AS_MOFF = 1587298080U;
+        static const AkUniqueID AS_MOON = 1738296607U;
+        static const AkUniqueID AS_SELECTED = 4277914999U;
         static const AkUniqueID BITE_EVENT = 3525025206U;
         static const AkUniqueID BREATHING_EVENT = 8016256U;
         static const AkUniqueID CHANGEMASTERVOLUME_EVENT = 1959847908U;
         static const AkUniqueID CHECKSWRONGAREA_EVENT = 3204241235U;
+        static const AkUniqueID CM_MOFF = 3584554960U;
+        static const AkUniqueID CM_MON = 2607239994U;
+        static const AkUniqueID CM_SELECTED = 2345214631U;
+        static const AkUniqueID CS_MOFF = 1580967594U;
+        static const AkUniqueID CS_MON = 3334221196U;
+        static const AkUniqueID CS_SELECTED = 1568591757U;
+        static const AkUniqueID D_MOFF = 3273167070U;
+        static const AkUniqueID D_MON = 4000350048U;
+        static const AkUniqueID D_SELECTED = 172843105U;
         static const AkUniqueID FIRE_EVENT = 2961143708U;
         static const AkUniqueID FLASHLIGHTOFF_EVENT = 897344331U;
         static const AkUniqueID FLASHLIGHTON_EVENT = 765301737U;
@@ -24,19 +39,57 @@ namespace AK
         static const AkUniqueID INTERACTPOSSIBILITY_EVENT = 2420961385U;
         static const AkUniqueID LEVEL0TUTORIALSTART_EVENT = 1746094278U;
         static const AkUniqueID LEVEL1START_EVENT = 3418008535U;
+        static const AkUniqueID LI_MOFF = 2793246759U;
+        static const AkUniqueID LI_MON = 2230557523U;
+        static const AkUniqueID LI_SELECTED = 3706057380U;
+        static const AkUniqueID MM_CLOSED = 1003940066U;
+        static const AkUniqueID MM_OPEN = 2209774454U;
         static const AkUniqueID MONSTER_EVENT = 3606884320U;
+        static const AkUniqueID MV_MOFF = 2191170055U;
+        static const AkUniqueID MV_MON = 1287208115U;
+        static const AkUniqueID MV_SELECTED = 3207148420U;
         static const AkUniqueID OUTOFBATTERY_EVENT = 4104297826U;
         static const AkUniqueID PLAYERDOESNTMOVE_EVENT = 2258719301U;
         static const AkUniqueID PLAYERESCAPE_EVENT = 36938082U;
         static const AkUniqueID PLAYERKILLED_EVENT = 1573989140U;
         static const AkUniqueID PLAYERNOMOVE_EVENT = 558371425U;
+        static const AkUniqueID RIM_MOFF = 2841767578U;
+        static const AkUniqueID RIM_MON = 1599513244U;
+        static const AkUniqueID RIM_SELECTED = 2088887037U;
         static const AkUniqueID RINGTONE_EVENT = 2005020198U;
+        static const AkUniqueID RMB_MOFF = 2590901651U;
+        static const AkUniqueID RMB_MON = 343846751U;
+        static const AkUniqueID RMB_SELECTED = 2676915216U;
+        static const AkUniqueID RMF_MOFF = 3276458479U;
+        static const AkUniqueID RMF_MON = 1212967419U;
+        static const AkUniqueID RMF_SELECTED = 1431111516U;
+        static const AkUniqueID RRL_MOFF = 3911281718U;
+        static const AkUniqueID RRL_MON = 2562839672U;
+        static const AkUniqueID RRL_SELECTED = 547275433U;
+        static const AkUniqueID RRR_MOFF = 360810964U;
+        static const AkUniqueID RRR_MON = 813278926U;
+        static const AkUniqueID RRR_SELECTED = 1849521131U;
+        static const AkUniqueID SB_MOFF = 4294671625U;
+        static const AkUniqueID SB_MON = 893021757U;
+        static const AkUniqueID SB_SELECTED = 4036155218U;
+        static const AkUniqueID SE_MOFF = 3578360856U;
+        static const AkUniqueID SE_MON = 3107127362U;
+        static const AkUniqueID SE_SELECTED = 3285578751U;
         static const AkUniqueID SPECIALOBJECT_EVENT = 2083017334U;
         static const AkUniqueID SPECIALOBJECTLEVEL2_EVENT = 2507613630U;
         static const AkUniqueID STOPRINGTONE_EVENT = 2989102206U;
         static const AkUniqueID STOPSPECIALOBJECT_EVENT = 4022243742U;
         static const AkUniqueID STOPSPECIALOBJECTLEVEL2_EVENT = 3754200486U;
         static const AkUniqueID TEST_EVENT = 4258653052U;
+        static const AkUniqueID TIF_MOFF = 2974502673U;
+        static const AkUniqueID TIF_MON = 1765325061U;
+        static const AkUniqueID TIF_SELECTED = 2653906634U;
+        static const AkUniqueID TTTS_MOFF = 1663727781U;
+        static const AkUniqueID TTTS_MON = 1218374169U;
+        static const AkUniqueID TTTS_SELECTED = 4207433118U;
+        static const AkUniqueID VS_MOFF = 241933055U;
+        static const AkUniqueID VS_MON = 846413483U;
+        static const AkUniqueID VS_SELECTED = 104785260U;
         static const AkUniqueID WALLBUMP_EVENT = 3664724136U;
     } // namespace EVENTS
 
@@ -52,6 +105,7 @@ namespace AK
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID TEST_SOUNDBANK = 4154996803U;
+        static const AkUniqueID VO_SOUNDBANK = 2861145642U;
     } // namespace BANKS
 
     namespace BUSSES

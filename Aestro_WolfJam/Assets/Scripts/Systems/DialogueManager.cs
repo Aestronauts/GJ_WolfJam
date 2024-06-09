ec8918953edc0b1e0cf7988a966fe51d69542154
Trisyd <gabrielpriem@gmail.com>
2024-06-08 22:51:26 -0700

    Dialogue Manager w/Narrator's Lines

--- Aestro_WolfJam/Assets/Scripts/Systems/DialogueManager.cs
+++ Aestro_WolfJam/Assets/Scripts/Systems/DialogueManager.cs
@@ -8,6 +8,15 @@ public class DialogueManager : MonoBehaviour
     public static DialogueManager instance;
     float NoDialogueCounter = 0;
 
+    //List<AK.Wwise.Event> WwiseDialogueEvents = new List<AK.Wwise.Event>();
+    public AK.Wwise.Event levelStartTutorial;
+    public AK.Wwise.Event levelStart1;
+    public AK.Wwise.Event checksWrongArea;
+    public AK.Wwise.Event playerDoesntMove;
+    public AK.Wwise.Event playerEscapes;
+    public AK.Wwise.Event playerKilled;
+    public AK.Wwise.Event playerIdles;
+
     private void Awake()
     {
         if (instance != null)
@@ -37,34 +46,34 @@ public class DialogueManager : MonoBehaviour
         {
             if (SceneManager.GetActiveScene().buildIndex == 0) // scene id needs to be set up but this should be level 0
             {
-
+                levelStartTutorial.Post(gameObject);
             }
             else if (SceneManager.GetActiveScene().buildIndex == 1) // scene id needs to be set up but this should be level 1
             {
-
+                levelStart1.Post(gameObject);
             }
         }
         else if (i == 2)
         {
-
+            playerIdles.Post(gameObject);
         }
         else if (i == 3)
         {
-
+            checksWrongArea.Post(gameObject);
         }
         else if (i == 4)
         {
-            Debug.Log("Don't Move Dialogue");
+            playerDoesntMove.Post(gameObject);
         }
         else if (i == 5)
         {
             if (SceneManager.GetActiveScene().buildIndex == 0) // scene id needs to be set up but this should be level 0
             {
-
+                playerEscapes.Post(gameObject);
             }
             else if (SceneManager.GetActiveScene().buildIndex == 1) // scene id needs to be set up but this should be level 1
             {
-
+                playerEscapes.Post(gameObject);
             }
         }
         //add more under

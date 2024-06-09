01c5495f0a90ecdc47c08fb601b97fc60d31985f
Trisyd <gabrielpriem@gmail.com>
2024-06-08 17:46:11 -0700

    Monster SFX Functionality

---
+++ Aestro_WolfJam/Assets/MonsterSFX.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSFX : MonoBehaviour
+{
+    public float radius = 10f;
+    public GameObject monster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M) == true)
+        {
+            Debug.Log("M pressed...");
+            SpawnMonster();
+        }
+    }
+
+    private void SpawnMonster()
+    {
+        Transform spawnLocation = gameObject.transform;
+        //Debug.Log("x: " + spawnLocation.transform.position.x);
+        //Debug.Log("y: " + spawnLocation.transform.position.y);
+        //Debug.Log("z: " + spawnLocation.transform.position.z);
+
+        float x = Random.Range(-10f, 10f);
+        Debug.Log("randomized x: " + x);
+
+        float radius_squared = radius * radius;
+        float x_squared = x * x;
+        float y = radius_squared - x_squared;
+        y = Mathf.Sqrt(y);
+        Debug.Log("calculated y: " + y);
+
+        float posneg = Random.Range(-1f, 1f);
+        Debug.Log("Random value for positive or negative: " + posneg);
+        if (posneg <= 0) y = -y;
+
+        Vector3 spawnLoc = new Vector3(spawnLocation.transform.position.x + x,
+            spawnLocation.transform.position.y,
+            spawnLocation.transform.position.z + y);
+
+        Debug.Log("SpawnLoc x: " + spawnLoc.x);
+        Debug.Log("SpawnLoc y: " + spawnLoc.y);
+        Debug.Log("SpawnLoc z: " + spawnLoc.z);
+
+        GameObject newMonster = Instantiate(monster, spawnLoc, transform.rotation);
+        newMonster.SetActive(true);
+    }
+}

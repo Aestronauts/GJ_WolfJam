56b32ab2c682cace2508046ee0c7af5a9fde1e2a
Noah <noahm1216@gmail.com>
2024-06-09 11:28:15 -0700

    Additional Tutorial Checkpoint

---
+++ Aestro_WolfJam/Assets/Scripts/Visuals/TutorialEvents.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEvents : MonoBehaviour
+{
+
+
+
+    private IEnumerator Tutorial()
+    {
+        yield return new WaitForSeconds(0);
+    }
+
+}// end of TutorialEvents class
